9772b905e509e9935e1b4e801a9b882630012741
prateektiwari711 <146694071+prateektiwari711@users.noreply.github.com>
2024-04-08 21:34:53 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import React, { Component } from 'react'
 import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import About from './pages/About';
 import Home from './pages/Home';
 import Business from './pages/Business';
